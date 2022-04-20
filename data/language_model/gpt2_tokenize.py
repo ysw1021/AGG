2478bb7b1821d702ec0b074f83cb976a6ead91fb
dbtkddnjs96 <dbtkddnjs96@bass.snu.ac.kr>
2022-04-20 14:09:56 +0900

    gpt2_tokenizer for wikitext preprocessing

---
+++ data/language_model/gpt2_tokenize.py
@@ -0,0 +1,82 @@
+from pytorch_transformers import GPT2Tokenizer
+import os
+
+tokenizer_gpt2 = GPT2Tokenizer.from_pretrained('gpt2')
+
+new_lines = []
+with open('wikitext-103/wiki.train.tokens', "r") as f:
+    worker_id = 0
+    size = os.fstat(f.fileno()).st_size
+    chunk_size = size
+    offset = worker_id * chunk_size
+    end = offset + chunk_size
+    f.seek(offset)
+    line = f.readline()
+    while line:
+        tokenized = tokenizer_gpt2.tokenize(line)
+        if tokenized[0] == '<|endoftext|>':
+            tokenized = []
+        tokenized.append('\n')
+        new_line = " ".join(tokenized)
+        new_lines.append(new_line)
+        if f.tell() > end:
+            break
+        line = f.readline()
+
+print()
+print('Writing preprocessed data file...')
+with open('wikitext-103/wiki.train.bpetokens', "w") as f:
+    for l in new_lines:
+        f.write(l)
+
+new_lines = []
+with open('wikitext-103/wiki.test.tokens', "r") as f:
+    worker_id = 0
+    size = os.fstat(f.fileno()).st_size
+    chunk_size = size
+    offset = worker_id * chunk_size
+    end = offset + chunk_size
+    f.seek(offset)
+    line = f.readline()
+    while line:
+        tokenized = tokenizer_gpt2.tokenize(line)
+        if tokenized[0] == '<|endoftext|>':
+            tokenized = []
+        tokenized.append('\n')
+        new_line = " ".join(tokenized)
+        new_lines.append(new_line)
+        if f.tell() > end:
+            break
+        line = f.readline()
+
+print()
+print('Writing preprocessed data file...')
+with open('wikitext-103/wiki.test.bpetokens', "w") as f:
+    for l in new_lines:
+        f.write(l)
+
+new_lines = []
+with open('wikitext-103/wiki.valid.tokens', "r") as f:
+    worker_id = 0
+    size = os.fstat(f.fileno()).st_size
+    chunk_size = size
+    offset = worker_id * chunk_size
+    end = offset + chunk_size
+    f.seek(offset)
+    line = f.readline()
+    while line:
+        tokenized = tokenizer_gpt2.tokenize(line)
+        if tokenized[0] == '<|endoftext|>':
+            tokenized = []
+        tokenized.append('\n')
+        new_line = " ".join(tokenized)
+        new_lines.append(new_line)
+        if f.tell() > end:
+            break
+        line = f.readline()
+
+print()
+print('Writing preprocessed data file...')
+with open('wikitext-103/wiki.valid.bpetokens', "w") as f:
+    for l in new_lines:
+        f.write(l)
